7905cc22f84448cc5e5110070863fb58856fde08
Kirill Streltsov <kirill.streltsov@7p-group.com>
2023-08-13 18:53:14 +0200

    Added a model and a cool navigation view with an animating button

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Streltsov, Kirill, SEVEN PRINCIPLES on 13.08.23.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let theme = ColorTheme()
+
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
