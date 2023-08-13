f2f6ec9b20ee017fa9d7a3b4d83f55663ee86d35
Kirill Streltsov <kirill.streltsov@7p-group.com>
2023-08-13 14:24:55 +0200

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Streltsov, Kirill, SEVEN PRINCIPLES on 13.08.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
